13bdf0b108cae4869f3742a65b5bbfada4031b3d
Batuhan Düzgün <batuhan.duzgun@std.yeditepe.edu.tr>
2020-03-01 16:15:59 +0300

    textAutomobile function moved to another class!

--- chapter1-objects-classes/src/chapter1/objects/classes/ProgramStarter.java
+++ chapter1-objects-classes/src/chapter1/objects/classes/ProgramStarter.java
@@ -1,28 +1,6 @@
 package chapter1.objects.classes;
 
 public class ProgramStarter {
-
-	public static void testAutomobile(Automobile automobile, GPS gps) {
-		
-		if(gps != null) {
-			gps.setCoordinate(35.23423f, 27.23423f);
-		}
-		
-		automobile.changeGps(gps);
-		
-		automobile.startEngine();
-		
-		System.out.println(automobile.incrementGear());
-		System.out.println(automobile.incrementGear());
-		System.out.println(automobile.incrementGear());
-		
-		System.out.println(automobile.decrementGear());
-		
-		String currentLocation = automobile.getCurrentLocation();
-		System.out.println(currentLocation);
-		
-		automobile.stopEngine();
-	}
 	
 	public static void main(String[] args) {
 		
@@ -33,8 +11,10 @@ public class ProgramStarter {
 
 		GPS siemensGps = new GPS();
 
-		testAutomobile(bmw, siemensGps);
-		testAutomobile(mercedes, null);
+		String result = String.valueOf(6);
+		
+		AutomobileTester.testAutomobile(bmw, siemensGps);
+		AutomobileTester.testAutomobile(mercedes, null);
 	}
 
 }
