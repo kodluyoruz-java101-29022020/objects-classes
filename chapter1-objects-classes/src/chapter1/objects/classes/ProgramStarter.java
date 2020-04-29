07cfb7ae2c25d3176ba3cf989928dd21a6e2f66d
Batuhan Düzgün <batuhan.duzgun@std.yeditepe.edu.tr>
2020-04-30 00:16:37 +0300

    chapter-1 objects&classes sample!
    Change-Id: I5db4a4e011caa88562d54f51a7583bf3d4176ea1

--- chapter1-objects-classes/src/chapter1/objects/classes/ProgramStarter.java
+++ chapter1-objects-classes/src/chapter1/objects/classes/ProgramStarter.java
@@ -1,20 +1,133 @@
 package chapter1.objects.classes;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.Scanner;
+
 public class ProgramStarter {
 	
-	public static void main(String[] args) {
+	private static int ipSegmentSize = 4;
+	private static String minIpAddress = "11.122.10.19";
+	private static String maxIpAddress = "11.122.10.50";
+	private static String ipTemplate = "11.122.10";
+	private static Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) throws ParseException {
 		
-		byte wheelCount = 4;
+// 11.122.10.19 - 11.122.10.50 IP Bloğu Subnet IP'leri ifade eder.
+// Scanner ile input alınacak
+// Eğer 11.122.10 ile başlayan bir IP değilse, ekrana "Geçersiz IP" yazın.
+// Eğer başlıyorsa girilen IP Subnetwork içindeki mi kontrol edin.
+// Eğer subnet IP bloğuna düşüyorsa ekrana "Developer IP: <Ekrandan IP>"
+// Değilse "Sistem Yöneticinize Başvurunuz!"
 		
-		Automobile bmw = new Automobile("BMW", "Black", wheelCount, 250000.0f);
-		Automobile mercedes = new Automobile("Mercedes", "White", wheelCount, 350000.0f);
+		
+		putIpDefinitionMenuOnScreen();
+		
+		scanner.close();
+	}
+	
+	public static void putIpDefinitionMenuOnScreen() {
+		
+		System.out.println("To Quit program, please press Q, To start press S");
+		String pressedButton = scanner.nextLine();
+		
+		while(!"Q".equalsIgnoreCase(pressedButton)) {
+			
+			defineDeveloperIpAddress();
+			System.out.println("Would you like to define another developer IP?");
+			System.out.println("(If Yes, press \"Y\", If not press \"Q\")");
+			pressedButton = scanner.nextLine();
+		}
+	}
+	
+	public static void defineDeveloperIpAddress() {
+		
+		String userDefinedIp = enterIpAddress();
+		boolean validIp = validateIpTemplate(userDefinedIp);
+		
+		if(validIp) {
+			String[] userIpSegments = splitIpSegments(userDefinedIp);
+			
+			boolean validSegmentSize = validateSegmentSize(userIpSegments);
+			
+			if(!validSegmentSize) {
+				System.out.println("Invalid IP segment size!");
+				return;
+			}
+			
+			int userLastSegment = extractLastItemOfArray(userIpSegments);
+			
+			String[] minIpSegments = splitIpSegments(minIpAddress);
+			int minLastSegment = extractLastItemOfArray(minIpSegments);
 
-		GPS siemensGps = new GPS();
+			String[] maxIpSegments = splitIpSegments(maxIpAddress);
+			int maxLastSegment = extractLastItemOfArray(maxIpSegments);
 
-		String result = String.valueOf(6);
+			if(isSubNetworkIp(userLastSegment, minLastSegment, maxLastSegment)) {
+				String formattedScreenText = 
+						String.format("Developer IP defined as %s", userDefinedIp);
+				System.out.println(formattedScreenText);
+			}
+			else{
+				System.out.println("Sistem yöneticinize başvurunuz!");
+			}
+		}
+		else {
+			System.out.println("Geçersiz IP!");
+		}
 		
-		AutomobileTester.testAutomobile(bmw, siemensGps);
-		AutomobileTester.testAutomobile(mercedes, null);
+	}
+	
+	public static String enterIpAddress() {
+		System.out.println("IP adresinizi giriniz: ");
+		return scanner.nextLine();
+	}
+	
+	public static boolean validateIpTemplate(String ipAddress) {
+		
+		if(ipAddress == null) {
+			return false;
+		}
+		
+		return ipAddress.startsWith(ipTemplate);
+	}
+	
+	public static boolean validateSegmentSize(String[] userIpSegments) {
+		
+		if(userIpSegments == null) {
+			return false;
+		}
+		
+		return ipSegmentSize == userIpSegments.length;
+	}
+	
+	public static String[] splitIpSegments(String ipAddress) {
+		
+		if(ipAddress == null) {
+			return new String[0];
+		}
+		
+		return ipAddress.split("\\.");
 	}
 
+	public static int extractLastItemOfArray(String[] ipSegments) {
+		
+		if(ipSegments.length > 0) {
+			String lastSegment = ipSegments[ipSegments.length - 1];
+			return Integer.valueOf(lastSegment);
+		}
+		
+		return -1;
+	}
+	
+	public static boolean isSubNetworkIp(int userLastSegment, int minLastSegment, int maxLastSegment) {
+		
+		return (userLastSegment >= minLastSegment && userLastSegment <= maxLastSegment);
+	}
+	
 }
